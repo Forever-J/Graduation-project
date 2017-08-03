879176187c7ce1a992fab3f3f9ca44088a7a1585
Forever-J <foreverj.2012@foxmail.com>
2017-08-04 01:57:40 +0800

    graduate

---
+++ dojo/test/testst.js
@@ -0,0 +1,10 @@
+﻿define(["dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMixin",  "dojo/text!./0401.html","dojo/dom"], function (declare, _WidgetBase, _TemplatedMixin,temstring,dom) {
+    return declare([_WidgetBase, _TemplatedMixin], {
+        templateString:
+           temstring,
+        onClick: function (evt) {
+            var t = dom.byId("Text1");
+            alert(t.value);
+        }
+    });
+});
---
+++ dojo/test/util.js
@@ -0,0 +1,7 @@
+﻿define(["dojo/dom"], function (dom) {
+    return {
+        setRed: function (id) {
+            dom.byId(id).style.color = "red";
+        }
+    };
+});
